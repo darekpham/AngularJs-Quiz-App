27e99453543bf27c2a9a9ba9331657b2dace5863
Dariusz Pham <Dariusz Pham>
2016-12-30 14:24:58 +0100

    add questionAnswered logic

--- js/controllers/quizCtrl.js
+++ js/controllers/quizCtrl.js
@@ -12,12 +12,38 @@
             vm.quizMetrics = quizMetrics;
             vm.dataService = dataService;
             vm.questionAnswered = questionAnswered;
+            vm.setActiveQuestion = setActiveQuestion;
             vm.activeQuestion = 0;
 
-            function questionAnswered () {
-                
+            var numQuestionsAnswered = 0;
+
+            
+            function setActiveQuestion () {
+                var breakOut = false;
+                var quizLength = dataService.quizQuestions.length - 1;
+
+                while (!breakOut) {
+                    vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+                    if (dataService.quizQuestions[vm.activeQuestion].selected === null) {
+                        breakOut = true;
+                    }
+                }
             }
+
+            function questionAnswered () {
+
+                var quizLength = dataService.quizQuestions.length;
+
+                if (dataService.quizQuestions[vm.activeQuestion].selected !== null) {
+                    numQuestionsAnswered ++;
+                    if (numQuestionsAnswered >= quizLength) {
+                        //finalise quiz
+                    }
+                }
+
+                vm.setActiveQuestion();
+            }
+
         }
-
-
 })();
