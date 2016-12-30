1882ca24af49cb62e2bdd2a915436385380e9404
Dariusz Pham <Dariusz Pham>
2016-12-30 17:03:03 +0100

    Finalising app

--- js/controllers/resultsCtrl.js
+++ js/controllers/resultsCtrl.js
@@ -11,13 +11,36 @@
             vm.quizMetrics = quizMetrics;
             vm.dataService = dataService;
             vm.getAnswerClass = getAnswerClass;
+            vm.setActiveQuestion = setActiveQuestion;
+            vm.calculatePerc = calculatePerc;
+            vm.reset = reset;
             vm.activeQuestion = 0;
+
+            function calculatePerc () {
+                return quizMetrics.numCorrect / dataService.quizQuestions.length * 100;
+            }
+
+            function setActiveQuestion (index) {
+                vm.setActiveQuestion = index;
+            }
             
             function getAnswerClass (index) {
                 if(index === quizMetrics.correctAnswers[vm.activeQuestion]){
                     return "bg-success";
                 }else if(index === dataService.quizQuestions[vm.activeQuestion].selected){
                     return "bg-danger";
+                }
+            }
+
+            function reset () {
+                quizMetrics.changeState('results', false);
+                quizMetrics.numCorrect = 0;
+
+                for (i = 0; i < dataService.quizQuestions.length; i++) {
+                    var data = dataService.quizQuestions[i];
+
+                    data.selected = null;
+                    data.correct = null;
                 }
             }
         }
